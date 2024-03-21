55f9c43563190f75e298bd3138ddc86cfbbdb1cb
Abdul Rehman Khan <khan456.tdm@gmail.com>
2024-03-21 14:18:13 +0500

    [✨] styled navbar

--- Components/Header/Nav.jsx
+++ Components/Header/Nav.jsx
@@ -3,13 +3,21 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <nav className='flex justify-between py-2 px-6 shadow-xl items-center'>
-      <a href="/"><img className='w-11 aspect-square' src="/icon.png" alt="favicon" /></a>
-      <div id="nav-links" className='flex justify-between gap-4'>
-        <Link href='/'>Home</Link>
-        <Link href='/post'>Post</Link>
+    <>
+    <div className='p-3 mb-2'>
+    <nav className='flex justify-between py-2 px-5 rounded-3xl shadow-2xl border bg-gradient-to-b from-white/[0.3] to-neutral-400/[0.3] border-white'>
+      <a href="/" className='flex items-center'>
+        <img className='w-11 aspect-square' src="/icon.png" alt="favicon" />
+        <p className='font-semibold text-lg'>YouTube Clipper</p>
+        </a>
+      <div id="nav-links" className='flex justify-between items-center gap-4'>
+        <Link className='px-6 py-2 rounded-3xl bg-[#F37852] text-white font-semibold' href='/'>Home</Link>
+        <Link className='px-6 py-2 font-semibold' href='/about'>About</Link>
+        <Link className='px-6 py-2 font-semibold' href=''>How it works</Link>
       </div>
-      <a className='text-white py-2 px-4 rounded-lg bg-blue-500' href="/api/auth/login">Login</a>
+      <a className='text-white px-6 py-2 rounded-3xl bg-[#F37852]' href="/api/auth/login">Login</a>
     </nav>
+    </div>
+    </>
   )
 }
